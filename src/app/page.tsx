e52d39a6e8389c50f460406b1bcd82f318d7ca8f
chobotek <mladek.milan@gmail.com>
2024-09-18 22:59:59 +0200

    chore: first version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,63 @@
-import Image from "next/image";
+'use client'
+import { useState } from 'react'
+import { PrimeNumber } from '@/utils/PrimeNumber'
+
+const isNumber = (num: number) => !isNaN(num)
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [prime, setPrime] = useState<string | null>(null)
+  const [primes, setPrimes] = useState<number[]>([])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handePrimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length === 0 || e.target.value.match(/^\d+$/)) {
+      setPrime(e.target.value)
+    }
+  }
+
+  return (
+    <div>
+      <div className={'flex flex-col items-center justify-center gap-8'}>
+        <form
+          onSubmit={(formEvent) => {
+            formEvent.preventDefault()
+            const data = new FormData(formEvent.currentTarget)
+            const num = parseInt(data.get('prime') as string)
+
+            if (isNumber(num)) {
+              const primeNumber = new PrimeNumber(num)
+              const numbers: number[] = []
+              for (const i of primeNumber) {
+                numbers.push(i)
+              }
+              setPrimes(numbers)
+            }
+          }}
+        >
+          <input
+            name={'prime'}
+            className={
+              'shadow-inner shadow-slate-400 text-black h-14 text-2xl px-3 bg-amber-100 border-none rounded-md outline outline-2 outline-purple-400 focus:outline-purple-600'
+            }
+            type={'text'}
+            value={prime ?? ''}
+            maxLength={5}
+            onChange={handePrimeChange}
+          />
+          <button type={'submit'}>Get my primes</button>
+        </form>
+        <div className={'w-full grid grid-cols primes gap-2 px-4 content-center'}>
+          {primes.map((primeNum) => (
+            <div
+              key={primeNum}
+              className={
+                'object-cover flex justify-center items-center px-4 h-12 bg-amber-200 rounded-md transition transition-300 hover:bg-amber-300'
+              }
+            >
+              <strong className={''}>{primeNum.toLocaleString('cs')}</strong>
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
-  );
+  )
 }
