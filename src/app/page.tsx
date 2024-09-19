878e33c53afd801e672b5cf745c844a179f8ad2e
chobotek <mladek.milan@gmail.com>
2024-09-19 21:15:12 +0200

    fix: added line chart fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function PrimeChecker() {
     lowerPrimes: number[]
     chartData: { x: number; y: number }[]
   } | null>(null)
-  const [key, setKey] = useState(0)
+  const [_, setKey] = useState(0)
 
   const handleConfirm = () => {
     if (typeof number === 'number') {
